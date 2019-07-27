40c695ad82b2b145e6e5f6c427de2ee173bbc6d7
SaiferGit <44334473+SaiferGit@users.noreply.github.com>
2019-07-27 22:39:48 +0600

    4th push

--- app/src/main/java/com/example/weebo/MainActivity.java
+++ app/src/main/java/com/example/weebo/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.weebo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar mToolbar; // for adding the toolbar
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // referencing variables with layout
+        mToolbar = (Toolbar) findViewById(R.id.main_page_toolbar);
+
+        // adding the toolbar
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Weebo"); // giving the title
+
     }
 }
