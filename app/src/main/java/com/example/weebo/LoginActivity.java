a51338ef3ba0440fb30c628cf1317fa918199fb8
SaiferGit <44334473+SaiferGit@users.noreply.github.com>
2019-07-31 14:23:03 +0600

    9th push
    31st july

--- app/src/main/java/com/example/weebo/LoginActivity.java
+++ app/src/main/java/com/example/weebo/LoginActivity.java
@@ -3,18 +3,39 @@ package com.example.weebo;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseUser;
+
+import org.w3c.dom.Text;
 
 public class LoginActivity extends AppCompatActivity {
 
 
     private FirebaseUser currentUser;
 
+    private Button loginButton, phoneloginbutton;
+    private EditText userEmail, userPassword;
+    private TextView needNewAccountLink, forgetPasswordlink;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // defining variables with layouts
+        InitializeFields();
+
+        // if a user doesn't have an account then
+        needNewAccountLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendUserToRegisterActivity();
+            }
+        });
     }
 
     @Override
@@ -31,6 +52,20 @@ public class LoginActivity extends AppCompatActivity {
     private void sendUserToMainActivity() {
         Intent mainIntent = new Intent(LoginActivity.this, MainActivity.class);
         startActivity(mainIntent);
+    }
+
+    private void sendUserToRegisterActivity() {
+        Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+        startActivity(registerIntent);
+    }
+
+    private void InitializeFields() {
+        loginButton = (Button) findViewById(R.id.login_button);
+        phoneloginbutton = (Button) findViewById(R.id.phone_login_button);
+        userEmail = (EditText) findViewById(R.id.login_email);
+        userPassword = (EditText) findViewById(R.id.login_password);
+        needNewAccountLink = (TextView) findViewById(R.id.need_new_account_link);
+        forgetPasswordlink = (TextView) findViewById(R.id.forget_password_link);
     }
 
 }
--- app/src/main/java/com/example/weebo/RegisterActivity.java
+++ app/src/main/java/com/example/weebo/RegisterActivity.java
@@ -1,13 +1,46 @@
 package com.example.weebo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private Button createAccountButton;
+    private EditText userEmail, userPassword;
+    private TextView alreadyHaveAccountLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        //defining variables with layouts
+        InitializeFields();
+
+        // if a user has an account already
+        alreadyHaveAccountLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendUserToLoginActivity();
+            }
+        });
+    }
+
+    private void sendUserToLoginActivity() {
+        Intent loginIntent = new Intent(RegisterActivity.this, LoginActivity.class);
+        startActivity(loginIntent);
+    }
+
+    private void InitializeFields() {
+        createAccountButton = (Button) findViewById(R.id.register_button);
+        userEmail = (EditText) findViewById(R.id.register_email);
+        userPassword = (EditText) findViewById(R.id.register_password);
+        alreadyHaveAccountLink = (TextView) findViewById(R.id.already_have_account_link);
+
     }
 }
