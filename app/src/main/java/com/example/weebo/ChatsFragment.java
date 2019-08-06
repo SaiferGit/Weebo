d21f4912398ba88486314c58743658977014e445
SaiferGit <44334473+SaiferGit@users.noreply.github.com>
2019-08-06 22:10:16 +0600

    17th push
    till 06th august

--- app/src/main/java/com/example/weebo/ChatsFragment.java
+++ app/src/main/java/com/example/weebo/ChatsFragment.java
@@ -1,17 +1,45 @@
 package com.example.weebo;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import org.w3c.dom.Text;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ChatsFragment extends Fragment {
+
+    private View privateChatsView;
+    private RecyclerView chatsList;
+
+    private DatabaseReference chatsRef, usersRef;
+    private FirebaseAuth mAuth;
+    private String currentUserID;
 
 
     public ChatsFragment() {
@@ -23,7 +51,110 @@ public class ChatsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chats, container, false);
+        privateChatsView = inflater.inflate(R.layout.fragment_chats, container, false);
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUserID = mAuth.getCurrentUser().getUid();
+        chatsRef = FirebaseDatabase.getInstance().getReference().child("Contacts").child(currentUserID);
+        usersRef = FirebaseDatabase.getInstance().getReference().child("Users");
+
+        chatsList = (RecyclerView) privateChatsView.findViewById(R.id.chats_list);
+        chatsList.setLayoutManager(new LinearLayoutManager(getContext()));
+
+
+        return privateChatsView;
     }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerOptions<Contacts> options =
+                new FirebaseRecyclerOptions.Builder<Contacts>()
+                .setQuery(chatsRef, Contacts.class)
+                .build();
+
+        FirebaseRecyclerAdapter<Contacts, ChatsViewHolder> adapter
+                = new FirebaseRecyclerAdapter<Contacts, ChatsViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull final ChatsViewHolder holder, int position, @NonNull Contacts model) {
+                final String usersIDs = getRef(position).getKey();
+                final String[] retImage = {"default_image"};
+
+                usersRef.child(usersIDs).addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        if(dataSnapshot.exists())
+                        {
+                            if(dataSnapshot.hasChild("image"))
+                            {
+                                retImage[0] = dataSnapshot.child("image").getValue().toString();
+                                Picasso.get().load(retImage[0]).placeholder(R.drawable.profile_image).into(holder.profileImage);
+
+                            }
+                            final String retName = dataSnapshot.child("name").getValue().toString();
+                            final String retstatus = dataSnapshot.child("status").getValue().toString();
+
+                            holder.userName.setText(retName);
+                            holder.userStatus.setText("Last Seen: " + "\n" + "Date " + " Time" );
+
+                            // when a user will click an item or user that is shown in chats fragment, the 1st user can click on any
+                            // position and can chat with the specific user.
+
+                            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    // sending user to chat activity
+                                    Intent chatIntent = new Intent(getContext(), ChatActivity.class);
+                                    chatIntent.putExtra("visit_user_id", usersIDs);
+                                    chatIntent.putExtra("visit_user_name", retName);
+                                    chatIntent.putExtra("visit_image", retImage[0]);
+
+                                    startActivity(chatIntent);
+                                }
+                            });
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+            }
+
+            @NonNull
+            @Override
+            public ChatsViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+
+                View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.users_display_layout, viewGroup, false);
+                return  new ChatsViewHolder(view);
+
+            }
+        };
+
+        chatsList.setAdapter(adapter);
+        adapter.startListening();
+    }
+
+
+    public static class ChatsViewHolder extends RecyclerView.ViewHolder
+    {
+        // includeing our layout in our static class
+        CircleImageView profileImage;
+        TextView userStatus, userName;
+
+        public ChatsViewHolder(@NonNull View itemView)
+        {
+            super(itemView);
+
+            profileImage = itemView.findViewById(R.id.users_profile_image);
+            userStatus = itemView.findViewById(R.id.user_status);
+            userName = itemView.findViewById(R.id.user_profile_name);
+
+        }
+    }
+
 
 }
---
+++ app/src/main/java/com/example/weebo/Messages.java
@@ -0,0 +1,40 @@
+package com.example.weebo;
+
+public class Messages
+{
+    // specifying our objects
+    private String from, message, type;
+
+    public Messages(String from, String message, String type) {
+        this.from = from;
+        this.message = message;
+        this.type = type;
+    }
+
+    public Messages() {
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
